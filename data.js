6c8db27b34b226e1aceb966aaaee01e86c79976a
TsuSh <107733731+TsuuSh@users.noreply.github.com>
2022-06-26 22:04:08 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+function dataRe(file, lines) {
+	fs.readFile(file, 'utf8', function(err, data) {
+		if (err) {
+			console.lof(err);
+		} else {
+		for (let i = 1; i <= lines; i++) {
+		fs.writeFile(file, data.replace(/\[\]/g, function() {
+			return "[" + i++ + "]";
+		}
+		), function(err) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log('Fichier liste.txt modifié');
+			}
+		});
+	}
+	}
+	});
+}
+
+dataRe('list.txt', 234);
